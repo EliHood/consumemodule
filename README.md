a59f619192f02853e8321b2f0060d998a94feb62
Eli <elihood@Elis-MacBook-Pro.local>
2019-07-01 14:35:27 -0500

    update

--- README.md
+++ README.md
@@ -16,9 +16,17 @@ import consumeModule from 'eliteamfetchmod';
 ```
 ### Method can be get or post
 
+
 ```diff
 consumeModule.get(url);
 ```
 
 ```diff
 consumeModule.post(url, data);
+```
+
+### For Post resquest example use
+https://jsonplaceholder.typicode.com/posts
+
+### For Get request example use
+https://jsonplaceholder.typicode.com/posts/1
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-// const myModule = require('./index');
+const myModule = require('./index');
 
-// const url = 'https://jsonplaceholder.typicode.com/posts/'
-// const value = {name:"BARN OWLS", job:'owling'}
-// const test = myModule.get(url, value);
+const url = 'https://jsonplaceholder.typicode.com/posts'
+const value = {name:"BARN OWLS", job:'owling'}
+const test = myModule.post(url, value);
 
-// console.log(test);
+console.log(test);
